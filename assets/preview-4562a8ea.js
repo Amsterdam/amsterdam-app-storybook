a547b0be3c76f674a98271b7f0cf8420a77b2452
Amsterdam CI <app@amsterdam.nl>
2023-10-26 15:38:13 +0000

    upload 0.40.0.6475

--- assets/preview-4562a8ea.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-c2aefd8c.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-7f5e99c1.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-7f5e99c1.js","./iframe-c2aefd8c.js","./index-e67e0a49.js","./_commonjsHelpers-de833af9.js","./_getPrototype-a04d3709.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-4562a8ea.js.map
