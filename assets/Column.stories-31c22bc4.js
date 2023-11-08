678e421930e415aff45f7b0264ffdbb30076f109
Amsterdam CI <app@amsterdam.nl>
2023-11-08 09:07:57 +0000

    upload 0.40.0.6587

--- assets/Column.stories-31c22bc4.js
+++
@@ -1,8 +0,0 @@
-import{j as r,a as m}from"./useThemable-05c0eaf9.js";import{C as t}from"./Column-5f32e9df.js";import{C as i,B as e}from"./Canvas-ea959c0a.js";import"./index-e67e0a49.js";import"./_commonjsHelpers-de833af9.js";import"./mapMainAxisAlignment-e13c2957.js";import"./index-df3ac05d.js";const f={component:t},a=n=>r(i,{highlight:!0,children:m(t,{...n,children:[r(e,{label:"A"}),r(e,{label:"B"}),r(e,{label:"C"})]})});a.args={align:"start",grow:!1,gutter:"md",halign:"stretch",reverse:!1};var s,o,l;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`args => <Canvas highlight>
-    <Column {...args}>
-      <Block label="A" />
-      <Block label="B" />
-      <Block label="C" />
-    </Column>
-  </Canvas>`,...(l=(o=a.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};const B=["Default"];export{a as Default,B as __namedExportsOrder,f as default};
-//# sourceMappingURL=Column.stories-31c22bc4.js.map
