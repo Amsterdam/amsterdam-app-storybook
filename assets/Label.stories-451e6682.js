30c0ff916db4611493722181f2385045c9017354
Amsterdam CI <app@amsterdam.nl>
2023-11-29 13:14:18 +0000

    upload 0.40.0.6688

--- assets/Label.stories-451e6682.js
+++
@@ -1,6 +0,0 @@
-import{L as o}from"./Label-c9526311.js";import"./useThemable-badc2311.js";import"./index-4ab02870.js";import"./_commonjsHelpers-de833af9.js";import"./Paragraph-5fb95601.js";import"./index-9d1af517.js";import"./index-8c71fb4d.js";const l={component:o},t={args:{text:"Wat is de titel van je bericht?"}};var e,r,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    text: 'Wat is de titel van je bericht?'
-  }
-}`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,l as default};
-//# sourceMappingURL=Label.stories-451e6682.js.map
---
+++ assets/preview-2d487e25.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-7635fe82.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-3fde030b.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-3fde030b.js","./iframe-7635fe82.js","./index-4ab02870.js","./_commonjsHelpers-de833af9.js","./_getPrototype-097c4360.js","./_commonjs-dynamic-modules-302442b1.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-2d487e25.js.map
