d7ec4597c4e4cb9b253ac953439a7b361d084b17
Amsterdam CI <app@amsterdam.nl>
2023-11-29 11:50:43 +0000

    upload 0.40.0.6685

--- assets/Pressable.stories-c88740d7.js
+++
@@ -1,7 +0,0 @@
-import{j as a}from"./useThemable-badc2311.js";import{P as t}from"./Pressable-7b49c5f6.js";import{P as i}from"./Paragraph-5fb95601.js";import"./index-4ab02870.js";import"./_commonjsHelpers-de833af9.js";import"./Box-c0ecbf9c.js";import"./layoutStyles-7b13d22c.js";import"./index-8c71fb4d.js";import"./index-4fdbf81e.js";import"./extends-98964cd2.js";import"./index-7e8d53f4.js";import"./index-9d1af517.js";const _={component:t,argTypes:{onPress:{action:"onPress"}}},r={args:{children:a(i,{children:"Press me"}),insetHorizontal:"lg"}};var o,s,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>Press me</Paragraph>,
-    insetHorizontal: 'lg'
-  }
-}`,...(e=(s=r.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const b=["Default"];export{r as Default,b as __namedExportsOrder,_ as default};
-//# sourceMappingURL=Pressable.stories-c88740d7.js.map
