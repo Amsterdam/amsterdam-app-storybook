b666b9763003372f7c80d7f9415620b7dfd8bd11
Amsterdam CI <app@amsterdam.nl>
2023-12-07 12:28:31 +0000

    upload 0.41.0.6753

---
+++ assets/Icon.stories-a7d11733.js
@@ -0,0 +1,7 @@
+import{I as e}from"./Icon-698fb629.js";import"./useThemable-0c29db81.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-6c192943.js";import"./index-9d475cdf.js";import"./Row-7183adb0.js";import"./mapMainAxisAlignment-e13c2957.js";import"./index-75ce51c7.js";import"./Animated-3046c943.js";import"./invariant-7bb126f5.js";import"./extends-98964cd2.js";import"./index-d96d58dd.js";import"./index-0fb53e87.js";import"./index-5b61bf2d.js";import"./index-ec5a4acc.js";import"./types-e7c584cc.js";import"./useDeviceContext-e004f4a0.js";import"./index-1597b195.js";import"./useTheme-4e1d74b9.js";const j={component:e},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    color: 'default',
+    name: 'announcement',
+    size: 'lg'
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const k=["Default"];export{o as Default,k as __namedExportsOrder,j as default};
---
+++ assets/Row.stories-1d631231.js
@@ -0,0 +1,19 @@
+import{j as r,a as g}from"./useThemable-0c29db81.js";import{B as o}from"./Button-19c20172.js";import{R as s}from"./Row-7183adb0.js";import{C as u,B as i}from"./Canvas-7515e7eb.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./config-55b2a55c.js";import"./Icon-698fb629.js";import"./index-6c192943.js";import"./index-9d475cdf.js";import"./Animated-3046c943.js";import"./invariant-7bb126f5.js";import"./extends-98964cd2.js";import"./index-75ce51c7.js";import"./index-d96d58dd.js";import"./index-0fb53e87.js";import"./index-5b61bf2d.js";import"./index-ec5a4acc.js";import"./types-e7c584cc.js";import"./useDeviceContext-e004f4a0.js";import"./index-1597b195.js";import"./useTheme-4e1d74b9.js";import"./index-ee60ff1c.js";import"./index-56eca6dc.js";import"./mapMainAxisAlignment-e13c2957.js";const H={component:s},e=t=>r(u,{highlight:!0,children:g(s,{...t,children:[r(i,{label:"A"}),r(i,{label:"B"}),r(i,{label:"C"})]})});e.args={align:"start",gutter:"md",wrap:!1};const a=t=>r(u,{highlight:!0,maxWidth:"24rem",children:g(s,{...t,children:[r(o,{label:"Wijzig adres"}),r(o,{label:"Voeg adres toe",variant:"secondary"}),r(o,{label:"Verwijder adres",variant:"secondary"})]})});a.args={gutter:"md",wrap:!0};var n,m,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`args => <Canvas highlight>
+    <Row {...args}>
+      <Block label="A" />
+      <Block label="B" />
+      <Block label="C" />
+    </Row>
+  </Canvas>`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,d,c;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`args => (
+/**
+ * Zet \`wrap\` aan om de items in een rij automatisch over meerdere regels te laten vallen.
+ * De \`gutter\` wordt dan gebruikt als verticale ruimte tussen de items.
+ * Je kunt de verticale ruimte tussen de items ook apart instellen met \`vgutter\`.
+ */
+<Canvas highlight maxWidth="24rem">
+    <Row {...args}>
+      <Button label="Wijzig adres" />
+      <Button label="Voeg adres toe" variant="secondary" />
+      <Button label="Verwijder adres" variant="secondary" />
+    </Row>
+  </Canvas>)`,...(c=(d=a.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const I=["Default","Wrapping"];export{e as Default,a as Wrapping,I as __namedExportsOrder,H as default};
