d6d8b4e23ac9ebf6218450f241996b481b902467
Amsterdam CI <app@amsterdam.nl>
2024-01-03 11:15:17 +0000

    upload 0.42.0.6825

---
+++ assets/Icon.stories-6b11e595.js
@@ -0,0 +1,7 @@
+import{I as e}from"./Icon-c27808bf.js";import"./useThemable-0a4280af.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-f3e2b6de.js";import"./index-9d475cdf.js";import"./Row-2788a98c.js";import"./mapMainAxisAlignment-e13c2957.js";import"./index-dd8453f7.js";import"./Animated-76626587.js";import"./invariant-b50f8bb2.js";import"./extends-98964cd2.js";import"./index-5403444a.js";import"./index-3d43cdb1.js";import"./index-9d6b7183.js";import"./index-75170bcc.js";import"./types-e7c584cc.js";import"./useDeviceContext-0bebe91d.js";import"./index-e9663dc2.js";import"./useTheme-874d5163.js";const j={component:e},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    color: 'default',
+    name: 'announcement',
+    size: 'lg'
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const k=["Default"];export{o as Default,k as __namedExportsOrder,j as default};
