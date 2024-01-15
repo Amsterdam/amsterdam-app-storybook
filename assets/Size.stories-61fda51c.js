9abfdc2d1bfb5ac6eda3cbe742f91c4504c7a453
Amsterdam CI <app@amsterdam.nl>
2024-01-15 16:02:54 +0000

    upload 0.42.0.6886

---
+++ assets/Size.stories-61fda51c.js
@@ -0,0 +1,5 @@
+import{j as e}from"./useThemable-f30bc085.js";import{S as i}from"./Size-806a3c11.js";import{P as m}from"./Phrase-bd7f1fae.js";import{C as n}from"./Canvas-1020ab0a.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-c2b660c2.js";import"./index-ccfb1f0c.js";const x={component:i},r=o=>e(i,{...o,children:e(n,{highlight:!0,children:e(m,{children:"Ik accepteer afmetingen"})})});r.args={maxWidth:128,minHeight:128};var a,t,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`args => <Size {...args}>
+    <Canvas highlight>
+      <Phrase>Ik accepteer afmetingen</Phrase>
+    </Canvas>
+  </Size>`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const z=["Default"];export{r as Default,z as __namedExportsOrder,x as default};
