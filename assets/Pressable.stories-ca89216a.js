f7feecd6c513d01cfaa2b3442e80f3fe14456b08
Amsterdam CI <app@amsterdam.nl>
2024-01-30 10:56:48 +0000

    upload 0.42.0.6945

---
+++ assets/Pressable.stories-ca89216a.js
@@ -0,0 +1,6 @@
+import{j as a}from"./useThemable-d55ba667.js";import{P as t}from"./Pressable-68f72adf.js";import{P as i}from"./Paragraph-c29c7b57.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./Box-4a6ffc0c.js";import"./layoutStyles-48d2a324.js";import"./index-f5687b4b.js";import"./index-da478e43.js";import"./extends-98964cd2.js";import"./index-56eca6dc.js";import"./index-3a34f86d.js";const b={component:t,argTypes:{onPress:{action:"onPress"}}},r={args:{children:a(i,{children:"Press me"}),insetHorizontal:"lg"}};var o,s,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>Press me</Paragraph>,
+    insetHorizontal: 'lg'
+  }
+}`,...(e=(s=r.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const j=["Default"];export{r as Default,j as __namedExportsOrder,b as default};
---
+++ assets/Timeline.stories-af63137c.js
@@ -0,0 +1,46 @@
+import{S as x,a as m,j as r,F as I}from"./useThemable-d55ba667.js";import{r as D}from"./index-76fb7be0.js";import{A as w}from"./Accordion-19da3b44.js";import{I as A}from"./Icon-89016759.js";import{H as _}from"./HtmlContent-73cbdfe6.js";import{T as z}from"./Title-6790b5f7.js";import{u as C}from"./useDeviceContext-b382c158.js";import{u as j}from"./useTheme-bf603680.js";import{V as l}from"./index-f5687b4b.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./Pressable-68f72adf.js";import"./Box-4a6ffc0c.js";import"./layoutStyles-48d2a324.js";import"./index-da478e43.js";import"./extends-98964cd2.js";import"./index-56eca6dc.js";import"./Column-f72089b4.js";import"./mapMainAxisAlignment-e13c2957.js";import"./Row-3b3483d9.js";import"./Size-9344bb82.js";import"./index-23b7c7a9.js";import"./index-a1161470.js";import"./index-9d475cdf.js";import"./Animated-db9b573c.js";import"./NativeEventEmitter-9324ed93.js";import"./index-a34fa860.js";import"./index-3a34f86d.js";import"./types-e7c584cc.js";import"./index-c0c54fbf.js";import"./index-c1b0849e.js";import"./index-a65b8874.js";import"./index-ef4fe7cd.js";import"./index-deb0b553.js";import"./index-f53fd744.js";import"./index-a66ad80e.js";import"./index-8ccb6f91.js";import"./index-8178225e.js";import"./index-54aef8c7.js";import"./Phrase-653177fd.js";import"./ListItemMarker-63b29f76.js";import"./config-55b2a55c.js";import"./useOpenMailUrl-5a6b63f0.js";import"./useSentry-04082daf.js";import"./logging-8e72dd05.js";import"./development-5f53618d.js";import"./useOpenPhoneUrl-26fd261b.js";import"./useIsScreenReaderEnabled-eb475990.js";const E=(o,n,e,i,c,u)=>{const{color:s,size:p}=o,t={body:{insetLeft:p.spacing.md},indicator:{size:p.spacing.lg*n},line:{width:4*n}};return x.create({header:{flexDirection:"row",justifyContent:"space-between",alignItems:"flex-start"},indicator:{position:"relative",width:t.indicator.size,aspectRatio:1,justifyContent:"center",alignItems:"center",top:p.spacing.md,backgroundColor:i?s.background.inactive:s.background.emphasis,borderRadius:t.indicator.size/2},line:{position:"absolute",top:p.spacing.md+t.indicator.size,left:(t.indicator.size-t.line.width)/2,zIndex:-1,width:t.line.width,height:u&&!c?0:"100%",backgroundColor:i||e?s.background.inactive:s.background.emphasis}})},g=({isBeforeUpcoming:o,isLast:n,item:e})=>{var f,b;const i=!e.collapsed,[c,u]=D.useState(i),s=j(),{fontScale:p}=C(),t=E(s,p,o,e.progress==="Aankomend",c,n);return m(l,{children:[m(l,{style:t.header,children:[r(l,{style:t.indicator,children:e.progress==="Afgelopen"&&r(A,{color:"inverse",name:"checkmark",size:"sm"})}),m(w,{grow:!0,initiallyExpanded:!!i,isExpandable:!!e.body||!!((f=e.items)!=null&&f.length),onChangeExpanded:a=>u(a),title:e.title,children:[!!e.body&&r(_,{content:e.body}),(b=e.items)==null?void 0:b.map(({title:a,body:h})=>m(D.Fragment,{children:[!!a&&r(z,{level:"h5",text:a}),!!h&&r(_,{content:h})]},a))]},e.title)]}),r(l,{style:t.line})]})};try{g.displayName="TimelineItem",g.__docgenInfo={description:"",displayName:"TimelineItem",props:{isBeforeUpcoming:{defaultValue:null,description:"",name:"isBeforeUpcoming",required:!0,type:{name:"boolean"}},isLast:{defaultValue:null,description:"",name:"isLast",required:!0,type:{name:"boolean"}},item:{defaultValue:null,description:"",name:"item",required:!0,type:{name:"ProjectTimelineItem"}}}}}catch{}const y=({items:o})=>r(I,{children:o.map((n,e)=>{var i;return r(g,{isBeforeUpcoming:((i=o[e+1])==null?void 0:i.progress)==="Aankomend",isLast:e===o.length-1,item:n},n.title+e.toString())})});try{y.displayName="Timeline",y.__docgenInfo={description:"",displayName:"Timeline",props:{items:{defaultValue:null,description:"",name:"items",required:!0,type:{name:"ProjectTimelineItem[]"}}}}}catch{}const we={component:y},d={args:{items:[{collapsed:!1,body:"<p>Dolor sit amet 1</p>",date:"",items:[{title:"Lorem ipsum 1.1",body:"<p>Dolor sit amet 1.1</p>",date:""},{title:"Lorem ipsum 1.2",body:"<p>Dolor sit amet 1.2</p>",date:""}],progress:"Afgelopen",title:"Titel 1"},{collapsed:!0,body:"<p>Dolor sit amet 2</p>",date:"",items:[{title:"Lorem ipsum 2.1",body:"<p>Dolor sit amet 2.1</p>",date:""}],progress:"Huidig",title:"Titel 2"},{collapsed:!0,body:"<p>Dolor sit amet 3</p>",date:"",items:[{title:"Lorem ipsum 3.1",body:"<p>Dolor sit amet 3.1</p>",date:""},{title:"Lorem ipsum 3.2",body:"<p>Dolor sit amet 3.2</p>",date:""}],progress:"Aankomend",title:"Titel 3"}]}};var T,L,k;d.parameters={...d.parameters,docs:{...(T=d.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      collapsed: false,
+      body: '<p>Dolor sit amet 1</p>',
+      date: '',
+      items: [{
+        title: 'Lorem ipsum 1.1',
+        body: '<p>Dolor sit amet 1.1</p>',
+        date: ''
+      }, {
+        title: 'Lorem ipsum 1.2',
+        body: '<p>Dolor sit amet 1.2</p>',
+        date: ''
+      }],
+      progress: 'Afgelopen',
+      title: 'Titel 1'
+    }, {
+      collapsed: true,
+      body: '<p>Dolor sit amet 2</p>',
+      date: '',
+      items: [{
+        title: 'Lorem ipsum 2.1',
+        body: '<p>Dolor sit amet 2.1</p>',
+        date: ''
+      }],
+      progress: 'Huidig',
+      title: 'Titel 2'
+    }, {
+      collapsed: true,
+      body: '<p>Dolor sit amet 3</p>',
+      date: '',
+      items: [{
+        title: 'Lorem ipsum 3.1',
+        body: '<p>Dolor sit amet 3.1</p>',
+        date: ''
+      }, {
+        title: 'Lorem ipsum 3.2',
+        body: '<p>Dolor sit amet 3.2</p>',
+        date: ''
+      }],
+      progress: 'Aankomend',
+      title: 'Titel 3'
+    }]
+  }
+}`,...(k=(L=d.parameters)==null?void 0:L.docs)==null?void 0:k.source}}};const Ae=["Default"];export{d as Default,Ae as __namedExportsOrder,we as default};
