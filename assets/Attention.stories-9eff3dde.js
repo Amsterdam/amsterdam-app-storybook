950cd0794839af86f1dda561ee82ef8c131447a2
Amsterdam CI <app@amsterdam.nl>
2024-02-28 10:35:28 +0000

    upload 0.43.0.7194

--- assets/Attention.stories-9eff3dde.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./index-1401012c.js";import{A as s}from"./Attention-17841f31.js";import{P as n}from"./Paragraph-12a38325.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-a5fed888.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-d86c6d3e.js";import"./base-color-0322ee5e.js";import"./index-e1b95cde.js";import"./TextAncestorContext-63ed7f56.js";import"./index-e39fa516.js";const A={component:s},r={args:{children:t(n,{children:"Er is iets misgegaan met de app. Sorry voor het ongemak!"}),warning:!1}};var a,o,e;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>
-        Er is iets misgegaan met de app. Sorry voor het ongemak!
-      </Paragraph>,
-    warning: false
-  }
-}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,A as default};
