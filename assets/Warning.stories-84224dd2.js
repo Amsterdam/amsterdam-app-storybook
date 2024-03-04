abb6191df8ac25f080b80e0cc4c103d2917d6c4c
Amsterdam CI <app@amsterdam.nl>
2024-03-04 12:44:31 +0000

    upload 0.43.0.7219

--- assets/Warning.stories-84224dd2.js
+++
@@ -1,6 +0,0 @@
-import{W as i}from"./Warning-efa7dac9.js";import"./index-1401012c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Attention-17841f31.js";import"./useThemable-a5fed888.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-d86c6d3e.js";import"./base-color-0322ee5e.js";import"./index-e1b95cde.js";import"./TextAncestorContext-63ed7f56.js";import"./Paragraph-3fde7a84.js";import"./index-e39fa516.js";import"./Title-3191e633.js";const W={component:i},r={args:{title:"Sorry …",text:"Er is iets misgegaan"}};var t,o,e;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    title: 'Sorry …',
-    text: 'Er is iets misgegaan'
-  }
-}`,...(e=(o=r.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,W as default};
