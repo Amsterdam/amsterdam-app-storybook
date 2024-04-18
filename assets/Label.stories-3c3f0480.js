4c24cae922d9b0c311fcbffc8183d583debef251
Amsterdam CI <app@amsterdam.nl>
2024-04-18 10:29:24 +0000

    upload 1.3.0.7480

--- assets/Label.stories-3c3f0480.js
+++
@@ -1,5 +0,0 @@
-import{L as a}from"./Label-6be100dd.js";import"./index-1401012c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Paragraph-7fd14d66.js";import"./useThemable-be20f09e.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-d86c6d3e.js";import"./base-color-0322ee5e.js";import"./index-e39fa516.js";import"./TextAncestorContext-63ed7f56.js";import"./index-e1b95cde.js";const g={component:a},t={args:{text:"Wat is de titel van je bericht?"}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    text: 'Wat is de titel van je bericht?'
-  }
-}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const L=["Default"];export{t as Default,L as __namedExportsOrder,g as default};
