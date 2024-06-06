c96e133063c33a170e77fb5be3709ade392c9699
Amsterdam CI <app@amsterdam.nl>
2024-06-06 12:59:47 +0000

    upload 1.4.0.7577

---
+++ assets/Phrase.stories-d9cd42e7.js
@@ -0,0 +1,15 @@
+import{a as l,j as s}from"./index-1401012c.js";import{P as e}from"./Phrase-31822a25.js";import{V as p}from"./index-e1b95cde.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-212e8c5a.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-1f08def5.js";import"./base-color-0322ee5e.js";import"./index-e39fa516.js";import"./TextAncestorContext-63ed7f56.js";const j={component:e},r={args:{children:"Phrase content",color:"default",emphasis:"default",variant:"body"}},a=()=>l(p,{children:[s(e,{emphasis:"strong",testID:"Phrase",variant:"small",children:"14"}),s(e,{testID:"Phrase",variant:"small",children:"volgers"})]});var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Phrase content',
+    color: 'default',
+    emphasis: 'default',
+    variant: 'body'
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var i,m,c;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`() => <View>
+    <Phrase emphasis="strong" testID="Phrase" variant="small">
+      14
+    </Phrase>
+    <Phrase testID="Phrase" variant="small">
+      volgers
+    </Phrase>
+  </View>`,...(c=(m=a.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const w=["Default","Pattern"];export{r as Default,a as Pattern,w as __namedExportsOrder,j as default};
