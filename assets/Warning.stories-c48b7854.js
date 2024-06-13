dcf06275c1f1b0bf8bc16b468d5115be3dba8ce6
Amsterdam CI <app@amsterdam.nl>
2024-06-13 08:02:19 +0000

    upload 1.5.0.7601

--- assets/Warning.stories-c48b7854.js
+++
@@ -1,6 +0,0 @@
-import{a as m,j as a}from"./index-1401012c.js";import{A as p}from"./Attention-4610b71b.js";import{P as l}from"./Paragraph-537ab1db.js";import{T as c}from"./Title-b90e8b4c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-030d6c49.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-1f08def5.js";import"./base-color-0322ee5e.js";import"./index-e1b95cde.js";import"./TextAncestorContext-63ed7f56.js";import"./index-e39fa516.js";const t=({text:e,title:s})=>m(p,{warning:!0,children:[a(c,{level:"h5",text:s}),a(l,{variant:"small",children:e})]});try{t.displayName="Warning",t.__docgenInfo={description:"",displayName:"Warning",props:{text:{defaultValue:null,description:"",name:"text",required:!0,type:{name:"string"}},title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}}}}}catch{}const v={component:t},r={args:{title:"Sorry …",text:"Er is iets misgegaan"}};var i,o,n;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    title: 'Sorry …',
-    text: 'Er is iets misgegaan'
-  }
-}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const A=["Default"];export{r as Default,A as __namedExportsOrder,v as default};
