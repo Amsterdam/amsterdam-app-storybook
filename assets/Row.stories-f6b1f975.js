e76ad959d9803309bd057e911c538fdf8b51fde0
Amsterdam CI <app@amsterdam.nl>
2024-06-17 14:11:38 +0000

    upload 1.5.0.7641

---
+++ assets/Row.stories-f6b1f975.js
@@ -0,0 +1,19 @@
+import{j as t}from"./index-9d7eb5a2.js";import{B as a}from"./Button-72fb9ead.js";import{R as s}from"./Row-1a77dc83.js";import{B as i}from"./Block-40768d62.js";import{C as c}from"./Canvas-f80a35a9.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./PressableBase-cc865898.js";import"./usePiwikTrackCustomEventFromProps-1034ddce.js";import"./slugs-b43e7e98.js";import"./index-6f107027.js";import"./NativeEventEmitter-7be6cb46.js";import"./invariant-f1a82cf7.js";import"./index-fb7b51b4.js";import"./TextAncestorContext-3b992cff.js";import"./development-de2bc6fd.js";import"./redux-toolkit.esm-cb9067f1.js";import"./useSentry-34852e81.js";import"./index-07bd7250.js";import"./callBound-c4e9bae7.js";import"./index-5008ca59.js";import"./extends-34e645d9.js";import"./index-b1a24850.js";import"./config-55b2a55c.js";import"./Icon-8065761e.js";import"./index-b21a90e4.js";import"./index-9d475cdf.js";import"./Animated-4476d759.js";import"./index-72552a59.js";import"./index-87b6cec9.js";import"./index-169dd17a.js";import"./types-65ac26c5.js";import"./useDeviceContext-82bee89b.js";import"./index-3cad2065.js";import"./useTheme-c7eb6588.js";import"./useThemable-e2fb66d4.js";import"./index-d3ea75b5.js";import"./base-color-0322ee5e.js";import"./mapCrossAxisAlignment-5d37f109.js";import"./mapMainAxisAlignment-c48b5fa8.js";const tt={component:s},r=o=>t.jsx(c,{highlight:!0,children:t.jsxs(s,{...o,children:[t.jsx(i,{label:"A"}),t.jsx(i,{label:"B"}),t.jsx(i,{label:"C"})]})});r.args={align:"start",gutter:"md",wrap:!1};const e=o=>t.jsx(c,{highlight:!0,maxWidth:"24rem",children:t.jsxs(s,{...o,children:[t.jsx(a,{label:"Wijzig adres",testID:"Button"}),t.jsx(a,{label:"Voeg adres toe",testID:"Button",variant:"secondary"}),t.jsx(a,{label:"Verwijder adres",testID:"Button",variant:"secondary"})]})});e.args={gutter:"md",wrap:!0};var m,n,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`args => <Canvas highlight>
+    <Row {...args}>
+      <Block label="A" />
+      <Block label="B" />
+      <Block label="C" />
+    </Row>
+  </Canvas>`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var l,d,u;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`args => (
+/**
+ * Zet \`wrap\` aan om de items in een rij automatisch over meerdere regels te laten vallen.
+ * De \`gutter\` wordt dan gebruikt als verticale ruimte tussen de items.
+ * Je kunt de verticale ruimte tussen de items ook apart instellen met \`vgutter\`.
+ */
+<Canvas highlight maxWidth="24rem">
+    <Row {...args}>
+      <Button label="Wijzig adres" testID="Button" />
+      <Button label="Voeg adres toe" testID="Button" variant="secondary" />
+      <Button label="Verwijder adres" testID="Button" variant="secondary" />
+    </Row>
+  </Canvas>)`,...(u=(d=e.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const rt=["Default","Wrapping"];export{r as Default,e as Wrapping,rt as __namedExportsOrder,tt as default};
