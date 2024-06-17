47b47ce939d3e13b29256b6152a6b8952b2278a8
Amsterdam CI <app@amsterdam.nl>
2024-06-17 11:55:01 +0000

    upload 1.5.0.7637

---
+++ assets/Attention.stories-26b79b13.js
@@ -0,0 +1,8 @@
+import{j as e}from"./index-9d7eb5a2.js";import{A as s}from"./Attention-3de13d04.js";import{P as n}from"./Paragraph-6b903c4c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-e2fb66d4.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-cb9067f1.js";import"./base-color-0322ee5e.js";import"./index-b1a24850.js";import"./TextAncestorContext-3b992cff.js";import"./index-169dd17a.js";const P={component:s},r={args:{children:e.jsx(n,{children:"Er is iets misgegaan met de app. Sorry voor het ongemak!"}),warning:!1}};var o,a,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Er is iets misgegaan met de app. Sorry voor het ongemak!
+      </Paragraph>,
+    warning: false
+  }
+}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const j=["Default"];export{r as Default,j as __namedExportsOrder,P as default};
---
+++ assets/Phrase.stories-e953e110.js
@@ -0,0 +1,15 @@
+import{j as s}from"./index-9d7eb5a2.js";import{P as a}from"./Phrase-57557c41.js";import{V as c}from"./index-b1a24850.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-e2fb66d4.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-cb9067f1.js";import"./base-color-0322ee5e.js";import"./index-169dd17a.js";import"./TextAncestorContext-3b992cff.js";const I={component:a},r={args:{children:"Phrase content",color:"default",emphasis:"default",variant:"body"}},e=()=>s.jsxs(c,{children:[s.jsx(a,{emphasis:"strong",testID:"Phrase",variant:"small",children:"14"}),s.jsx(a,{testID:"Phrase",variant:"small",children:"volgers"})]});var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Phrase content',
+    color: 'default',
+    emphasis: 'default',
+    variant: 'body'
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var i,m,p;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`() => <View>
+    <Phrase emphasis="strong" testID="Phrase" variant="small">
+      14
+    </Phrase>
+    <Phrase testID="Phrase" variant="small">
+      volgers
+    </Phrase>
+  </View>`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const V=["Default","Pattern"];export{r as Default,e as Pattern,V as __namedExportsOrder,I as default};
