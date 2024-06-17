7b80e504d055a84c2fd5d770e6c422c84b3f7ac1
Amsterdam CI <app@amsterdam.nl>
2024-06-17 11:56:14 +0000

    upload 1.5.0.7628

--- assets/Icon.stories-d4a6bf7e.js
+++
@@ -1,7 +0,0 @@
-import{I as p}from"./Icon-8065761e.js";import"./index-9d7eb5a2.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-b21a90e4.js";import"./index-9d475cdf.js";import"./Row-1a77dc83.js";import"./mapCrossAxisAlignment-5d37f109.js";import"./mapMainAxisAlignment-c48b5fa8.js";import"./useThemable-e2fb66d4.js";import"./index-d3ea75b5.js";import"./redux-toolkit.esm-cb9067f1.js";import"./base-color-0322ee5e.js";import"./index-b1a24850.js";import"./TextAncestorContext-3b992cff.js";import"./Animated-4476d759.js";import"./invariant-f1a82cf7.js";import"./extends-34e645d9.js";import"./index-72552a59.js";import"./NativeEventEmitter-7be6cb46.js";import"./index-87b6cec9.js";import"./index-169dd17a.js";import"./types-65ac26c5.js";import"./useDeviceContext-82bee89b.js";import"./index-6f107027.js";import"./index-fb7b51b4.js";import"./index-3cad2065.js";import"./useTheme-c7eb6588.js";const B={component:p},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    color: 'default',
-    name: 'announcement',
-    size: 'lg'
-  }
-}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const C=["Default"];export{o as Default,C as __namedExportsOrder,B as default};
