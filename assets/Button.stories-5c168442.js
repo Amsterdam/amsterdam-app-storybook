807f5d054d88d47e9d87b69d5791b8113e3f0228
Amsterdam CI <app@amsterdam.nl>
2024-07-03 12:22:26 +0000

    upload 1.5.0.7755

---
+++ assets/Button.stories-5c168442.js
@@ -0,0 +1,46 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{B as t}from"./Button-1c4cea24.js";import{R as p}from"./Row-7c1d2ebc.js";import{p as A}from"./pressable-arg-types-18907f68.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./PressableBase-cab79413.js";import"./usePiwikTrackCustomEventFromProps-e4307c50.js";import"./version-ba873cb0.js";import"./index-a464b0de.js";import"./NativeEventEmitter-723dff5d.js";import"./index-ac976766.js";import"./TextAncestorContext-dcad3dbc.js";import"./index-f819f4d6.js";import"./development-f8096310.js";import"./index-e8e5dbb3.js";import"./useTrackException-bb6f0470.js";import"./index-3ba17d29.js";import"./callBound-0c193d85.js";import"./___vite-browser-external_commonjs-proxy-698e55bb.js";import"./index-9ddf3bb5.js";import"./extends-4c19d496.js";import"./index-2bdd647f.js";import"./config-55b2a55c.js";import"./Icon-fbf1260b.js";import"./index-953b234c.js";import"./index-9d475cdf.js";import"./Animated-7a7ece98.js";import"./index-26b34527.js";import"./index-d1c3279d.js";import"./types-65ac26c5.js";import"./useDeviceContext-91140f6c.js";import"./index-fbb44dd0.js";import"./useTheme-7da6666b.js";import"./useThemable-dabb7424.js";import"./index-d3ea75b5.js";import"./base-color-0322ee5e.js";import"./mapCrossAxisAlignment-5d37f109.js";import"./mapMainAxisAlignment-c48b5fa8.js";const Br={argTypes:A,component:t,parameters:{componentSubtitle:"Het basis button component"}},o={args:{label:"Primary",variant:"primary"}},n={args:{label:"Secondary",variant:"secondary"}},s={args:{label:"Tertiary",variant:"tertiary"}},i={args:{align:"start"},render:({align:e})=>r.jsx(p,{align:e,children:r.jsx(t,{label:"Inline button",testID:"Button"})})},m={args:{align:"start",gutter:"md"},render:({align:e,gutter:P})=>r.jsxs(p,{align:e,gutter:P,children:[r.jsx(t,{label:"First button",testID:"Button"}),r.jsx(t,{label:"Second button",testID:"Button",variant:"secondary"}),r.jsx(t,{label:"Third button",testID:"Button",variant:"tertiary"})]})},a={args:{iconName:"phone",label:"Bel ons"},render:e=>r.jsx(p,{align:"start",children:r.jsx(t,{...e,testID:"Button"})})};var c,l,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Primary',
+    variant: 'primary'
+  }
+}`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,g,b;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...(b=(g=n.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var y,B,I;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Tertiary',
+    variant: 'tertiary'
+  }
+}`,...(I=(B=s.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var S,x,_;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    align: 'start'
+  },
+  render: ({
+    align
+  }) => <Row align={align}>
+      <Button label="Inline button" testID="Button" />
+    </Row>
+}`,...(_=(x=i.parameters)==null?void 0:x.docs)==null?void 0:_.source}}};var j,v,D;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    align: 'start',
+    gutter: 'md'
+  },
+  render: ({
+    align,
+    gutter
+  }) => <Row align={align} gutter={gutter}>
+      <Button label="First button" testID="Button" />
+      <Button label="Second button" testID="Button" variant="secondary" />
+      <Button label="Third button" testID="Button" variant="tertiary" />
+    </Row>
+}`,...(D=(v=m.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};var h,R,T,w,f;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    iconName: 'phone',
+    label: 'Bel ons'
+  },
+  render: args => <Row align="start">
+      <Button {...args} testID="Button" />
+    </Row>
+}`,...(T=(R=a.parameters)==null?void 0:R.docs)==null?void 0:T.source},description:{story:"Knop met een icoon",...(f=(w=a.parameters)==null?void 0:w.docs)==null?void 0:f.description}}};const Ir=["Primary","Secondary","Tertiary","_Inline","__Alignment","__hasIcon"];export{o as Primary,n as Secondary,s as Tertiary,i as _Inline,m as __Alignment,a as __hasIcon,Ir as __namedExportsOrder,Br as default};
---
+++ assets/Icon.stories-09b54f9d.js
@@ -0,0 +1,7 @@
+import{I as p}from"./Icon-fbf1260b.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-953b234c.js";import"./index-9d475cdf.js";import"./index-f819f4d6.js";import"./Row-7c1d2ebc.js";import"./mapCrossAxisAlignment-5d37f109.js";import"./mapMainAxisAlignment-c48b5fa8.js";import"./useThemable-dabb7424.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./base-color-0322ee5e.js";import"./index-2bdd647f.js";import"./TextAncestorContext-dcad3dbc.js";import"./Animated-7a7ece98.js";import"./index-ac976766.js";import"./extends-4c19d496.js";import"./NativeEventEmitter-723dff5d.js";import"./index-26b34527.js";import"./index-d1c3279d.js";import"./types-65ac26c5.js";import"./useDeviceContext-91140f6c.js";import"./index-a464b0de.js";import"./index-fbb44dd0.js";import"./useTheme-7da6666b.js";const A={component:p},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    color: 'default',
+    name: 'announcement',
+    size: 'lg'
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const B=["Default"];export{o as Default,B as __namedExportsOrder,A as default};
