2c24b1584fcc70d979940f06c6917b84ad0e1f00
Amsterdam CI <app@amsterdam.nl>
2024-07-03 10:21:11 +0000

    upload 1.5.0.7751

--- assets/Attention.stories-8896f44d.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-ffb262ed.js";import{A as s}from"./Attention-cb5554e4.js";import{P as i}from"./Paragraph-e886c9de.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-dabb7424.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./index-f819f4d6.js";import"./base-color-0322ee5e.js";import"./index-2bdd647f.js";import"./TextAncestorContext-dcad3dbc.js";import"./index-d1c3279d.js";const j={component:s},r={args:{children:e.jsx(i,{children:"Er is iets misgegaan met de app. Sorry voor het ongemak!"}),warning:!1}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>
-        Er is iets misgegaan met de app. Sorry voor het ongemak!
-      </Paragraph>,
-    warning: false
-  }
-}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const A=["Default"];export{r as Default,A as __namedExportsOrder,j as default};
