5a9c5d8894e7a554d2deef56216fa20ae4fe5992
Amsterdam CI <app@amsterdam.nl>
2024-07-11 12:42:10 +0000

    upload 1.6.0.7772

--- assets/Attention.stories-09e6aff0.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-ffb262ed.js";import{A as s}from"./Attention-6acac861.js";import{P as i}from"./Paragraph-610c0bb4.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-e82c2842.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./index-f819f4d6.js";import"./base-color-07bc5405.js";import"./index-2bdd647f.js";import"./TextAncestorContext-dcad3dbc.js";import"./index-d1c3279d.js";const j={component:s},r={args:{children:e.jsx(i,{children:"Er is iets misgegaan met de app. Sorry voor het ongemak!"}),warning:!1}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>
-        Er is iets misgegaan met de app. Sorry voor het ongemak!
-      </Paragraph>,
-    warning: false
-  }
-}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const A=["Default"];export{r as Default,A as __namedExportsOrder,j as default};
---
+++ assets/Label.stories-c7226b5b.js
@@ -0,0 +1,5 @@
+import{L as a}from"./Label-77273bb1.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Paragraph-2450cab5.js";import"./useThemable-e82c2842.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./index-f819f4d6.js";import"./base-color-07bc5405.js";import"./index-d1c3279d.js";import"./TextAncestorContext-dcad3dbc.js";import"./index-2bdd647f.js";const L={component:a},t={args:{text:"Wat is de titel van je bericht?"}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    text: 'Wat is de titel van je bericht?'
+  }
+}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,L as default};
