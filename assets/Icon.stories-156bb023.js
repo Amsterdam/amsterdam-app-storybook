cdd5bfaf8d8a57b102bfa53c889f05d04be3f83a
Amsterdam CI <app@amsterdam.nl>
2024-07-30 10:07:04 +0000

    upload 1.6.0.7871

--- assets/Icon.stories-156bb023.js
+++
@@ -1,7 +0,0 @@
-import{I as p}from"./Icon-71d4c053.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-ecf17355.js";import"./index-9d475cdf.js";import"./index-a802de3b.js";import"./Row-6bfdd943.js";import"./mapCrossAxisAlignment-5d37f109.js";import"./mapMainAxisAlignment-c48b5fa8.js";import"./useThemable-89dffd9c.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./base-color-f0c30692.js";import"./index-5b56d0c8.js";import"./TextAncestorContext-c1a63433.js";import"./Animated-fdc153e4.js";import"./index-115efad7.js";import"./extends-4c19d496.js";import"./NativeEventEmitter-2071600e.js";import"./index-4f48d35c.js";import"./index-ccab62b4.js";import"./types-65ac26c5.js";import"./useDeviceContext-d0c458db.js";import"./index-ee304789.js";import"./index-e8db3c07.js";import"./useTheme-8aa51487.js";const A={component:p},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    color: 'default',
-    name: 'announcement',
-    size: 'lg'
-  }
-}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const B=["Default"];export{o as Default,B as __namedExportsOrder,A as default};
