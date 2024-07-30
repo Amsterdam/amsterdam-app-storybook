72d2a3e970b6d6596643416114e1c8e75c5aac14
Amsterdam CI <app@amsterdam.nl>
2024-07-30 13:00:34 +0000

    upload 1.6.0.7885

--- assets/useTheme-8aa51487.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-76fb7be0.js";import{e as s,D as t}from"./useThemable-89dffd9c.js";const a=()=>{const{theme:e}=s(t);return r.useMemo(()=>e,[e])};export{a as u};
---
+++ assets/useTheme-e8898c27.js
@@ -0,0 +1,1 @@
+import{r}from"./index-76fb7be0.js";import{e as s,E as t}from"./useThemable-ae3b5f69.js";const a=()=>{const{theme:e}=s(t);return r.useMemo(()=>e,[e])};export{a as u};
