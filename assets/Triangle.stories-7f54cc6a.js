b473b8ef420e107bb1b770e4814fc7320d374b4d
Amsterdam CI <app@amsterdam.nl>
2024-07-31 17:30:29 +0000

    upload 1.6.0.7891

---
+++ assets/Triangle.stories-7f54cc6a.js
@@ -0,0 +1,5 @@
+import{T as e}from"./Triangle-583931ed.js";import{D as m}from"./types-65ac26c5.js";import"./jsx-runtime-ffb262ed.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-ecf17355.js";import"./index-9d475cdf.js";import"./index-a802de3b.js";import"./Center-34f5926c.js";import"./layoutStyles-b0426a14.js";import"./index-5b56d0c8.js";import"./TextAncestorContext-c1a63433.js";import"./useThemable-3442ebd9.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./base-color-f0c30692.js";const O={component:e},r={args:{direction:m.right}};var o,t,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    direction: Direction.right
+  }
+}`,...(i=(t=r.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,O as default};
