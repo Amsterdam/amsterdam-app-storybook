3e5df4a88c93880174664c6cb40586fcda434ca0
Amsterdam CI <app@amsterdam.nl>
2024-08-13 07:36:22 +0000

    upload 1.6.0.7934

--- assets/Pressable.stories-3e14cbde.js
+++
@@ -1,6 +0,0 @@
-import{j as i}from"./jsx-runtime-ffb262ed.js";import{P as m}from"./Pressable-251b1f18.js";import{P as p}from"./Paragraph-489340f6.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./PressableBase-a08d7dc5.js";import"./usePiwikTrackCustomEventFromProps-0a34c2af.js";import"./index-e8e5dbb3.js";import"./useTrackException-a3338c13.js";import"./index-ee304789.js";import"./NativeEventEmitter-2071600e.js";import"./index-115efad7.js";import"./TextAncestorContext-c1a63433.js";import"./index-a802de3b.js";import"./development-6fd8b346.js";import"./index-e87daf6f.js";import"./callBound-db640f75.js";import"./index-075eace8.js";import"./extends-4c19d496.js";import"./index-5b56d0c8.js";import"./Box-cb0e605a.js";import"./layoutStyles-b0426a14.js";import"./useThemable-a987c5ae.js";import"./index-d3ea75b5.js";import"./base-color-f0c30692.js";import"./index-ccab62b4.js";const v={component:m,argTypes:{onPress:{action:"onPress"}}},r={args:{children:i.jsx(p,{children:"Press me"}),insetHorizontal:"lg"}};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: <Paragraph>Press me</Paragraph>,
-    insetHorizontal: 'lg'
-  }
-}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const w=["Default"];export{r as Default,w as __namedExportsOrder,v as default};
