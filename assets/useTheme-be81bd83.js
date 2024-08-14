bcd9c3ab50e681c899ef1eb09e61bfc4741c04be
Amsterdam CI <app@amsterdam.nl>
2024-08-14 13:05:39 +0000

    upload 1.6.0.7952

---
+++ assets/useTheme-be81bd83.js
@@ -0,0 +1,1 @@
+import{r}from"./index-76fb7be0.js";import{d as s,E as t}from"./useThemable-5c0dd8c3.js";const a=()=>{const{theme:e}=s(t);return r.useMemo(()=>e,[e])};export{a as u};
--- assets/useTheme-ef4f7e5a.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-76fb7be0.js";import{d as s,E as t}from"./useThemable-a987c5ae.js";const a=()=>{const{theme:e}=s(t);return r.useMemo(()=>e,[e])};export{a as u};
