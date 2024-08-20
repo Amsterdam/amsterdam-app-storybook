6389605227073d1207709834a25b08a235bad870
Amsterdam CI <app@amsterdam.nl>
2024-08-20 10:12:04 +0000

    upload 1.6.0.20240820.2

---
+++ assets/Attention.stories-a56ad47e.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{A as s}from"./Attention-a399c440.js";import{P as i}from"./Paragraph-19a6db22.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./useThemable-5c0dd8c3.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./index-a802de3b.js";import"./base-color-f0c30692.js";import"./index-5b56d0c8.js";import"./TextAncestorContext-c1a63433.js";import"./index-ccab62b4.js";const j={component:s},r={args:{children:e.jsx(i,{children:"Er is iets misgegaan met de app. Sorry voor het ongemak!"}),warning:!1}};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>
+        Er is iets misgegaan met de app. Sorry voor het ongemak!
+      </Paragraph>,
+    warning: false
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const A=["Default"];export{r as Default,A as __namedExportsOrder,j as default};
