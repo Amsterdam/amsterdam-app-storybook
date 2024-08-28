6c8270507c92b6158f4c61d8e457d8e139dd4111
Amsterdam CI <app@amsterdam.nl>
2024-08-28 11:23:06 +0000

    upload 1.7.0.20240828.1

---
+++ assets/Size.stories-67ada6a5.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{S as i}from"./Size-c5ad4b40.js";import{P as m}from"./Phrase-8a29f4b0.js";import"./Block-3b3f8b17.js";import{C as p}from"./Canvas-ac341b8f.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./mapMainAxisAlignment-c48b5fa8.js";import"./index-5b56d0c8.js";import"./index-a802de3b.js";import"./TextAncestorContext-c1a63433.js";import"./useThemable-70b6a0c1.js";import"./index-d3ea75b5.js";import"./index-e8e5dbb3.js";import"./base-color-5d90126d.js";import"./index-ccab62b4.js";const v={component:i},r=o=>e.jsx(i,{...o,children:e.jsx(p,{highlight:!0,children:e.jsx(m,{testID:"Phrase",children:"Ik accepteer afmetingen"})})});r.args={maxWidth:128,minHeight:128};var t,s,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`args => <Size {...args}>
+    <Canvas highlight>
+      <Phrase testID="Phrase">Ik accepteer afmetingen</Phrase>
+    </Canvas>
+  </Size>`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,v as default};
