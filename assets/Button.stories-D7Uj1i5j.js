338945d60189cf070c3915eb1c954a5594281838
Amsterdam CI <app@amsterdam.nl>
2024-09-19 12:30:25 +0000

    upload 1.7.0.20240919.3

---
+++ assets/Button.stories-D7Uj1i5j.js
@@ -0,0 +1,46 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{B as t}from"./Button-ynS4jfvk.js";import{R as p}from"./Row-Csjk4iRt.js";import{p as A}from"./pressable-arg-types-CJIMfHep.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PressableBase-DZHExAmZ.js";import"./usePiwikTrackCustomEventFromProps-ByXbYGhF.js";import"./index-8JwjhRSi.js";import"./useTrackException-YJ53Vokp.js";import"./index-zj3cuDFB.js";import"./NativeEventEmitter-DdN3StA5.js";import"./index-DVCMAXez.js";import"./TextAncestorContext-CxUfpxkU.js";import"./index-Us49mgpR.js";import"./development-Cx_QPvdM.js";import"./index-BKHiHeki.js";import"./extends-CF3RwP-h.js";import"./index-BySreB3I.js";import"./config-BZnShabN.js";import"./Icon-uC7AMkHJ.js";import"./index-Ddv-N6EM.js";import"./index-Dk74W0Oi.js";import"./Animated-DWDoN2T2.js";import"./index-BUR9LkGU.js";import"./index-BwZbmMJB.js";import"./types-BJ_48vsK.js";import"./useDeviceContext-CYreQoQV.js";import"./index-NoE4hmAe.js";import"./useTheme-CCdQLXM4.js";import"./useThemable-B5KlIRSX.js";import"./index-BtM5VmRH.js";import"./base-color-Co9_lLmz.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";const dr={argTypes:A,component:t,parameters:{componentSubtitle:"Het basis button component"}},n={args:{label:"Primary",variant:"primary"}},o={args:{label:"Secondary",variant:"secondary"}},s={args:{label:"Tertiary",variant:"tertiary"}},i={args:{align:"start"},render:({align:e})=>r.jsx(p,{align:e,children:r.jsx(t,{label:"Inline button",testID:"Button"})})},m={args:{align:"start",gutter:"md"},render:({align:e,gutter:P})=>r.jsxs(p,{align:e,gutter:P,children:[r.jsx(t,{label:"First button",testID:"Button"}),r.jsx(t,{label:"Second button",testID:"Button",variant:"secondary"}),r.jsx(t,{label:"Third button",testID:"Button",variant:"tertiary"})]})},a={args:{iconName:"phone",label:"Bel ons"},render:e=>r.jsx(p,{align:"start",children:r.jsx(t,{...e,testID:"Button"})})};var c,l,u;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Primary',
+    variant: 'primary'
+  }
+}`,...(u=(l=n.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,g,b;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Secondary',
+    variant: 'secondary'
+  }
+}`,...(b=(g=o.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var y,B,I;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Tertiary',
+    variant: 'tertiary'
+  }
+}`,...(I=(B=s.parameters)==null?void 0:B.docs)==null?void 0:I.source}}};var S,x,_;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    align: 'start'
+  },
+  render: ({
+    align
+  }) => <Row align={align}>
+      <Button label="Inline button" testID="Button" />
+    </Row>
+}`,...(_=(x=i.parameters)==null?void 0:x.docs)==null?void 0:_.source}}};var j,v,D;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    align: 'start',
+    gutter: 'md'
+  },
+  render: ({
+    align,
+    gutter
+  }) => <Row align={align} gutter={gutter}>
+      <Button label="First button" testID="Button" />
+      <Button label="Second button" testID="Button" variant="secondary" />
+      <Button label="Third button" testID="Button" variant="tertiary" />
+    </Row>
+}`,...(D=(v=m.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};var h,R,T,w,f;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    iconName: 'phone',
+    label: 'Bel ons'
+  },
+  render: args => <Row align="start">
+      <Button {...args} testID="Button" />
+    </Row>
+}`,...(T=(R=a.parameters)==null?void 0:R.docs)==null?void 0:T.source},description:{story:"Knop met een icoon",...(f=(w=a.parameters)==null?void 0:w.docs)==null?void 0:f.description}}};const gr=["Primary","Secondary","Tertiary","_Inline","__Alignment","__hasIcon"];export{n as Primary,o as Secondary,s as Tertiary,i as _Inline,m as __Alignment,a as __hasIcon,gr as __namedExportsOrder,dr as default};
