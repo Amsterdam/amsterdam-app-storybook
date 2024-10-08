2fce18520d17460984a827f86af87fba2865ced1
Amsterdam CI <app@amsterdam.nl>
2024-10-08 09:27:44 +0000

    upload 1.7.0.20241008.2

---
+++ assets/Icon.stories-DPBXIphH.js
@@ -0,0 +1,7 @@
+import{I as p}from"./Icon-DcC-yiY8.js";import"./jsx-runtime-Bxcv0yjW.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DYSHDArR.js";import"./index-Dk74W0Oi.js";import"./index-CEE--Juu.js";import"./Row-BXpfxHoR.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./useThemable-CjWxbHxI.js";import"./index-BMk6Tmu2.js";import"./index-8JwjhRSi.js";import"./base-color-CZpuL6yQ.js";import"./index-BDsvWohO.js";import"./TextAncestorContext-Cvz4PlE2.js";import"./Animated-C23eizOh.js";import"./index-DOpK3Ow6.js";import"./extends-CF3RwP-h.js";import"./NativeEventEmitter-Cj1pQcwn.js";import"./index-DcZXOGQR.js";import"./index-BypDj3rK.js";import"./types-BJ_48vsK.js";import"./useDeviceContext-DGl4H2w7.js";import"./index-Bcpfmd17.js";import"./index-C3bJM-gU.js";import"./useTheme-CJIBCVMY.js";const A={component:p},o={args:{color:"default",name:"announcement",size:"lg"}};var r,t,m;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    color: 'default',
+    name: 'announcement',
+    size: 'lg'
+  }
+}`,...(m=(t=o.parameters)==null?void 0:t.docs)==null?void 0:m.source}}};const B=["Default"];export{o as Default,B as __namedExportsOrder,A as default};
--- assets/Label.stories-CtOoJG1O.js
+++
@@ -1,5 +0,0 @@
-import{L as a}from"./Label-Bc-mTvQ-.js";import"./jsx-runtime-Bxcv0yjW.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Paragraph-CKYtcIcp.js";import"./useThemable-CjWxbHxI.js";import"./index-BMk6Tmu2.js";import"./index-8JwjhRSi.js";import"./index-CEE--Juu.js";import"./base-color-CZpuL6yQ.js";import"./index-BypDj3rK.js";import"./TextAncestorContext-Cvz4PlE2.js";import"./index-BDsvWohO.js";const L={component:a},t={args:{text:"Wat is de titel van je bericht?"}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    text: 'Wat is de titel van je bericht?'
-  }
-}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,L as default};
