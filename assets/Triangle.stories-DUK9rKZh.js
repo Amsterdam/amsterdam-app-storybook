00ab5643c9f6c758025d9dbee1de4eb00c236971
Amsterdam CI <app@amsterdam.nl>
2024-10-10 13:20:01 +0000

    upload 1.7.0.20241010.3

--- assets/Triangle.stories-DUK9rKZh.js
+++
@@ -1,5 +0,0 @@
-import{T as e}from"./Triangle-Dcpw5Pfc.js";import{D as a}from"./types-BJ_48vsK.js";import"./jsx-runtime-Bxcv0yjW.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DYSHDArR.js";import"./index-Dk74W0Oi.js";import"./index-CEE--Juu.js";import"./useThemable-DKzFKfGj.js";import"./index-BMk6Tmu2.js";import"./index-8JwjhRSi.js";import"./base-color-B2rPH-Aj.js";const _={component:e},r={args:{direction:a.right}};var o,t,i;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    direction: Direction.right
-  }
-}`,...(i=(t=r.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,_ as default};
