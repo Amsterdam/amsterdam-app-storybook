672e007a04de33b1e8c4c1c30acea6f3b6837d83
Amsterdam CI <app@amsterdam.nl>
2024-10-17 14:13:07 +0000

    upload 1.8.0.20241017.4

---
+++ assets/Label.stories-D-Z0PVHy.js
@@ -0,0 +1,5 @@
+import{L as a}from"./Label-1w6VMSaI.js";import"./jsx-runtime-Bxcv0yjW.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Paragraph-DrsqDZjR.js";import"./useThemable-DK_xoxH7.js";import"./index-BMk6Tmu2.js";import"./index-8JwjhRSi.js";import"./index-CEE--Juu.js";import"./base-color-B2rPH-Aj.js";import"./index-BypDj3rK.js";import"./TextAncestorContext-Cvz4PlE2.js";import"./index-BDsvWohO.js";const L={component:a},t={args:{text:"Wat is de titel van je bericht?"}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    text: 'Wat is de titel van je bericht?'
+  }
+}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,L as default};
