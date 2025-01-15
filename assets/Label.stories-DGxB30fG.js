03cd38cc72c4422017fed5840a9a87390b3833d0
Amsterdam CI <app@amsterdam.nl>
2025-01-15 14:34:24 +0000

    upload 1.10.0.20250115.3

--- assets/Label.stories-DGxB30fG.js
+++
@@ -1,5 +0,0 @@
-import{L as a}from"./Label-Cb3ozbEy.js";import"./jsx-runtime-Bxcv0yjW.js";import"./index-mfbfsM12.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Paragraph-F_Ci9u0t.js";import"./useThemable-C3my05Yx.js";import"./index-DFkxGuuM.js";import"./index-8JwjhRSi.js";import"./index-kbHxBZ_W.js";import"./base-color-B2rPH-Aj.js";import"./index-CAyeAlm6.js";import"./TextAncestorContext-Dg3XKfar.js";import"./index-BP1Ij_Cs.js";const L={component:a},t={args:{text:"Wat is de titel van je bericht?"}};var r,e,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    text: 'Wat is de titel van je bericht?'
-  }
-}`,...(o=(e=t.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,L as default};
--- assets/useTheme-b30gdhU3.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index-mfbfsM12.js";import{a as r,s as t}from"./useThemable-C3my05Yx.js";const a=()=>{const{theme:e}=r(t);return s.useMemo(()=>e,[e])};export{a as u};
