f8b80a2c119eba79a2c1c3bc18a7c73ecaca3407
Amsterdam CI <app@amsterdam.nl>
2025-01-23 16:02:16 +0000

    upload 1.10.0.20250123.5

--- assets/ChatSystemEntry.stories-BThPDyiD.js
+++
@@ -1,8 +0,0 @@
-import{C as p}from"./ChatSystemEntry-zdCxYZhb.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Row-DZZkXmAw.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./useThemable-Co7yv1NC.js";import"./index-D5xlL9g-.js";import"./index-8JwjhRSi.js";import"./index-BRs9ZavV.js";import"./base-color-B2rPH-Aj.js";import"./index-DNzWYFc8.js";import"./TextAncestorContext-CgUd_yOy.js";import"./Icon-BMYzK8wb.js";import"./index-D_f9P5ND.js";import"./index-Dk74W0Oi.js";import"./Animated-BCjaTVhM.js";import"./index-C9LjRz3B.js";import"./extends-CF3RwP-h.js";import"./NativeEventEmitter-CdIviC7q.js";import"./index-BNEt8Bkx.js";import"./index-CglSVHKQ.js";import"./types-BJ_48vsK.js";import"./useDeviceContext-BXdagwJr.js";import"./index-B9HBLp1U.js";import"./index-B5FlGKE6.js";import"./useTheme-Cf9EgLJc.js";import"./Phrase-DNGK9BSw.js";const z={component:p},t={args:{icon:"chat",text:"Chat gestopt",testID:"chatEntryRoutingWorkResult",timestamp:1630510440}};var r,o,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    icon: 'chat',
-    text: 'Chat gestopt',
-    testID: 'chatEntryRoutingWorkResult',
-    timestamp: 1630510440
-  }
-}`,...(m=(o=t.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const A=["Default"];export{t as Default,A as __namedExportsOrder,z as default};
