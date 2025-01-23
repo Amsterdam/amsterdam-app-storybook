9635bb3c7bd9f748dde0ba801e919f7f6ff352b6
Amsterdam CI <app@amsterdam.nl>
2025-01-23 08:31:30 +0000

    upload 1.10.0.20250123.2

---
+++ assets/Pressable.stories-D2vATpRc.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{P as i}from"./Pressable-ChtzqfZX.js";import{P as m}from"./Paragraph-DgH_NcQY.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./PressableBase-JFXJBQf5.js";import"./usePiwikTrackCustomEventFromProps-Dzlw6j1p.js";import"./index-8JwjhRSi.js";import"./useTrackException-BSsPgnnq.js";import"./index-B9HBLp1U.js";import"./NativeEventEmitter-CdIviC7q.js";import"./index-C9LjRz3B.js";import"./TextAncestorContext-CgUd_yOy.js";import"./index-BRs9ZavV.js";import"./development-B3zcR1jo.js";import"./index-1EE2wIxA.js";import"./extends-CF3RwP-h.js";import"./index-DNzWYFc8.js";import"./Box-lT9VwbH6.js";import"./layoutStyles-nv7cBgSD.js";import"./useThemable-Co7yv1NC.js";import"./index-D5xlL9g-.js";import"./base-color-B2rPH-Aj.js";import"./index-CglSVHKQ.js";const k={component:i,argTypes:{onPress:{action:"onPress"}}},r={args:{children:e.jsx(m,{children:"Press me"}),insetHorizontal:"lg"}};var o,t,s;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: <Paragraph>Press me</Paragraph>,
+    insetHorizontal: 'lg'
+  }
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const q=["Default"];export{r as Default,q as __namedExportsOrder,k as default};
