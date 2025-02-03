2d18c5ef09eb464dc23c4c934a3fe631a1673cf3
Amsterdam CI <app@amsterdam.nl>
2025-02-03 13:13:38 +0000

    upload 1.11.0.20250203.3

---
+++ assets/Column.stories-owyMvjF0.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-BlAj40OV.js";import{C as l}from"./Column-Ci0y-vuz.js";import{B as t}from"./Block-BzJc7EDD.js";import{C as i}from"./Canvas-C1Lfq4FF.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./useThemable-CDPjGmtb.js";import"./index-D5xlL9g-.js";import"./index-8JwjhRSi.js";import"./index-BRs9ZavV.js";import"./base-color-B2rPH-Aj.js";import"./index-DNzWYFc8.js";import"./TextAncestorContext-CgUd_yOy.js";const _={component:l},o=m=>r.jsx(i,{highlight:!0,children:r.jsxs(l,{...m,children:[r.jsx(t,{label:"A"}),r.jsx(t,{label:"B"}),r.jsx(t,{label:"C"})]})});o.args={align:"start",gutter:"md",halign:"stretch",reverse:!1};var s,a,e;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`args => <Canvas highlight>
+    <Column {...args}>
+      <Block label="A" />
+      <Block label="B" />
+      <Block label="C" />
+    </Column>
+  </Canvas>`,...(e=(a=o.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};const A=["Default"];export{o as Default,A as __namedExportsOrder,_ as default};
