8475a5560b5e38c54e67a22f766b5ec203fa5835
Amsterdam CI <app@amsterdam.nl>
2025-03-07 11:36:26 +0000

    upload 1.12.0.20250307.2

---
+++ assets/Image.stories-k1-YFjds.js
@@ -0,0 +1,6 @@
+import{I as a}from"./Image-CtzXjip9.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./development-B3zcR1jo.js";import"./index-C9LjRz3B.js";import"./TextAncestorContext-CgUd_yOy.js";import"./index-BRs9ZavV.js";import"./index-BNEt8Bkx.js";import"./extends-CF3RwP-h.js";import"./index-DNzWYFc8.js";import"./useThemable-CAFOynuf.js";import"./index-D5xlL9g-.js";import"./index-8JwjhRSi.js";import"./base-color-B2rPH-Aj.js";import"./index-B5FlGKE6.js";const m=""+new URL("screenshot-afvalwijzer-BVPHaBeJ.png",import.meta.url).href,I={component:a},r={args:{aspectRatio:"wide",source:m}};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    aspectRatio: 'wide',
+    source
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,I as default};
