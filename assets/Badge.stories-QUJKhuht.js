8c6cabcbfec2bc68eafd3e876d22470da5436bf9
Amsterdam CI <app@amsterdam.nl>
2025-03-24 12:26:42 +0000

    upload 1.13.0.20250324.1

--- assets/Badge.stories-QUJKhuht.js
+++
@@ -1,9 +0,0 @@
-import{B as d}from"./Badge-C1_QCmX3.js";import"./jsx-runtime-BlAj40OV.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Row-B8r3JqHk.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./useThemable-BtBcDdBB.js";import"./index-D5xlL9g-.js";import"./index-8JwjhRSi.js";import"./index-BRs9ZavV.js";import"./base-color-B2rPH-Aj.js";import"./index-DNzWYFc8.js";import"./TextAncestorContext-CgUd_yOy.js";import"./AccessibleText-COgu9Gly.js";import"./index-CglSVHKQ.js";import"./useDeviceContext-BXdagwJr.js";import"./index-B9HBLp1U.js";import"./NativeEventEmitter-CdIviC7q.js";import"./index-C9LjRz3B.js";import"./index-B5FlGKE6.js";const C={component:d,args:{value:3}},r={},o={args:{variant:"small"}},a={args:{color:"info"}};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};var m,p,i;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    variant: 'small'
-  }
-}`,...(i=(p=o.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var c,n,l;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    color: 'info'
-  }
-}`,...(l=(n=a.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const F=["Default","Small","Info"];export{r as Default,a as Info,o as Small,F as __namedExportsOrder,C as default};
