a0f1d69e7d820c63db7033be598241797b9d427e
Amsterdam CI <app@amsterdam.nl>
2025-04-15 09:38:29 +0000

    upload 1.13.0.20250415.1

--- assets/useGetCurrentParkingPermit.web-CZRE4cAJ.js
+++
@@ -1,1 +0,0 @@
-import{p as r}from"./permit.mock-wFLwxqzY.js";const t=()=>({currentPermit:r,isLoading:!1});export{t as u};
---
+++ assets/useGetCurrentParkingPermit.web-DB6Dmx7Y.js
@@ -0,0 +1,1 @@
+import{p as r}from"./permit.mock-pJ3ucmRl.js";const t=()=>({currentPermit:r,isLoading:!1});export{t as u};
