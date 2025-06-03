decbb5aae62444d93478facc8e52bf4db6380685
Amsterdam CI <app@amsterdam.nl>
2025-06-03 12:06:09 +0000

    upload 1.14.0.20250603.4

---
+++ assets/TextInput.stories-C3kiWVhV.js
@@ -0,0 +1,15 @@
+import{T as n}from"./TextInput-BuTAwLfy.js";import"./jsx-runtime-DNHyKP0h.js";import"./index-B-xgG2PE.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./IconButton-B8E0ehbf.js";import"./Pressable-DyM9EPlf.js";import"./PressableBase-CyOB0T19.js";import"./usePiwikTrackCustomEventFromProps-LMgsJqJM.js";import"./index-CnyDuYXe.js";import"./useTrackException-dTAYkf2v.js";import"./index-aqbATsDD.js";import"./NativeEventEmitter-CjF4_R2K.js";import"./TextAncestorContext-Z5dgoe9Q.js";import"./index-Cm4QxXoK.js";import"./index-BiinuClr.js";import"./index-Cju5Ng7p.js";import"./extends-CF3RwP-h.js";import"./index-DoMT7H_C.js";import"./Box-BXURLcLs.js";import"./layoutStyles-Beptdp9x.js";import"./useThemable-BoIQ4zZO.js";import"./index-DICU_UuR.js";import"./index-CGHx0_JJ.js";import"./base-color-BCCzFoQm.js";import"./config-BZnShabN.js";import"./Badge-zpqaKVT7.js";import"./Row-DWZxcsB8.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./AccessibleText-ySoCsV7u.js";import"./index-CXGG-aYW.js";import"./useDeviceContext-BsghPmC5.js";import"./index-C70cEofB.js";import"./formatNumber-BOnMa6yj.js";import"./types-CttlCamk.js";import"./Label-_ueUni-Y.js";import"./Phrase-CsQE0k57.js";import"./Column-lHxSrgd8.js";import"./Icon-pf8ziu-7.js";import"./index-s4hYfBOP.js";import"./index-Chjiymov.js";import"./Animated-45BN0duk.js";import"./index-DoCTQWjN.js";import"./index-u-pG-eXf.js";import"./useTheme-Cc0Zgb7Z.js";import"./index-shQJSbQb.js";const rt={component:n},t={args:{label:"Wat is de titel van je bericht?",placeholder:"Voer een titel in...",value:""}},r={args:{label:"Wat is de titel van je bericht?",multiline:!0,numberOfLines:5,placeholder:"Voer een titel in...",value:""}};var e,i,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    label: 'Wat is de titel van je bericht?',
+    placeholder: 'Voer een titel in...',
+    value: ''
+  }
+}`,...(o=(i=t.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var p,m,a;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Wat is de titel van je bericht?',
+    multiline: true,
+    numberOfLines: 5,
+    placeholder: 'Voer een titel in...',
+    value: ''
+  }
+}`,...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};const et=["Default","Multiline"];export{t as Default,r as Multiline,et as __namedExportsOrder,rt as default};
