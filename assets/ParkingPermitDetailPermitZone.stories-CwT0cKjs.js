f42ba5d9f223297994924a07536b016ea48a07fd
Amsterdam CI <app@amsterdam.nl>
2025-06-23 15:26:52 +0000

    upload 1.14.0.20250623.8

---
+++ assets/ParkingPermitDetailPermitZone.stories-CwT0cKjs.js
@@ -0,0 +1,5 @@
+import{p}from"./permit.mock-CZwTJ9EP.js";import{P as i}from"./ParkingPermitDetailPermitZone-DhWF8Ox8.js";import"./types-C2OYdTo_.js";import"./jsx-runtime-DNHyKP0h.js";import"./index-B-xgG2PE.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Column-DV_vpfR5.js";import"./mapCrossAxisAlignment-DpnuLLqj.js";import"./mapMainAxisAlignment-BOa0GzUh.js";import"./useThemable-DhqiuDXa.js";import"./index-DICU_UuR.js";import"./index-CGHx0_JJ.js";import"./index-CnyDuYXe.js";import"./index-Cm4QxXoK.js";import"./base-color-BCCzFoQm.js";import"./index-DoMT7H_C.js";import"./TextAncestorContext-Z5dgoe9Q.js";import"./Phrase-Ohwx_RNQ.js";import"./AccessibleText-ySoCsV7u.js";import"./index-CXGG-aYW.js";import"./Title-DZoLJ8Fx.js";const h={component:i},r={args:{permit:p}};var t,o,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    permit: permitMock
+  }
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const j=["Default"];export{r as Default,j as __namedExportsOrder,h as default};
