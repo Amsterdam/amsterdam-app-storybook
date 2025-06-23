cc9e7ceeb671e3d6707231dc75e819092887fb9d
Amsterdam CI <app@amsterdam.nl>
2025-06-23 14:37:26 +0000

    upload 1.14.0.20250623.7

--- assets/CharactersLeftDisplay.stories-BJdtVpRs.js
+++
@@ -1,6 +0,0 @@
-import{C as e}from"./CharactersLeftDisplay-PdRR1k-d.js";import"./jsx-runtime-DNHyKP0h.js";import"./index-B-xgG2PE.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Paragraph-B2tgND3_.js";import"./AccessibleText-ySoCsV7u.js";import"./index-CXGG-aYW.js";import"./index-Cm4QxXoK.js";import"./TextAncestorContext-Z5dgoe9Q.js";import"./useThemable-DhqiuDXa.js";import"./index-DICU_UuR.js";import"./index-CGHx0_JJ.js";import"./index-CnyDuYXe.js";import"./base-color-BCCzFoQm.js";const g={component:e},r={args:{maxCharacters:50,numOfCharacters:0}};var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    maxCharacters: 50,
-    numOfCharacters: 0
-  }
-}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const O=["Default"];export{r as Default,O as __namedExportsOrder,g as default};
