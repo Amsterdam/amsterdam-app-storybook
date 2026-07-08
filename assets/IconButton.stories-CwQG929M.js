7443e3a15f23b9de3b543400a1d48dca1f52db22
Amsterdam CI <app@amsterdam.nl>
2026-07-08 14:41:00 +0000

    upload 1.28.0.26070807

--- assets/IconButton.stories-CwQG929M.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{t}from"./jsx-runtime-BBQGix-2.js";import{n,t as r}from"./Icon-CdA3InKl.js";import{n as i,t as a}from"./IconButton-D4aW0cdH.js";import{n as o,t as s}from"./pressable-arg-types-ao0z9Ssu.js";var c,l,u,d;e((()=>{i(),n(),s(),c=t(),l={component:a,argTypes:o},u={args:{icon:(0,c.jsx)(r,{isFilled:!0,name:`person`,size:`lg`,testID:`Icon`}),badgeValue:7}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
-  args: {
-    icon: <Icon isFilled name="person" size="lg" testID="Icon" />,
-    badgeValue: 7
-  }
-}`,...u.parameters?.docs?.source}}},d=[`Default`]}))();export{u as Default,d as __namedExportsOrder,l as default};
