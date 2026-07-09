a2b088454c8d984587c9ba083ef64646752f062d
Amsterdam CI <app@amsterdam.nl>
2026-07-09 09:27:39 +0000

    upload 1.29.0.26070902

---
+++ assets/ErrorMessage.stories-Bwx4Tz2d.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./ErrorMessage-Yyk19c1G.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{testID:`testIDMessage`,text:`Vul een waarde in`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    testID: 'testIDMessage',
+    text: 'Vul een waarde in'
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
