581d2bb798f5ec09d04d99f248bf84fcf74cb414
Amsterdam CI <app@amsterdam.nl>
2026-07-09 14:31:45 +0000

    upload 1.29.0.26070906

---
+++ assets/Triangle.stories-B9xr2aVi.js
@@ -0,0 +1,5 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{Ot as t,Pt as n}from"./iframe-DgfRA9Ch.js";import{n as r,t as i}from"./Triangle-BwaUjeyu.js";var a,o,s;e((()=>{r(),n(),a={component:i},o={args:{direction:t.right}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    direction: Direction.right
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`]}))();export{o as Default,s as __namedExportsOrder,a as default};
