315c97a18039924377c3c6ed190a7ff97d5b1c68
Amsterdam CI <app@amsterdam.nl>
2026-07-16 08:44:20 +0000

    upload 1.29.0.26071602

---
+++ assets/Badge.stories-CV8ceuEQ.js
@@ -0,0 +1,9 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./Badge-DI-VPTlo.js";var r,i,a,o,s;e((()=>{t(),r={component:n,args:{value:3}},i={},a={args:{variant:`small`}},o={args:{color:`info`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: 'small'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: 'info'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`Small`,`Info`]}))();export{i as Default,o as Info,a as Small,s as __namedExportsOrder,r as default};
--- assets/formatter-L2YTDE3Q-C3icw5Qj.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{vn as t,yn as n}from"./iframe-CEfrkY_h.js";import{i as r,n as i,r as a,t as o}from"./chunk-3LY4VQVK-CpkJ3RXe.js";var s;e((()=>{a(),i(),n(),s=(0,t(r(),1).default)(2)(async(e,t)=>e===!1?t:o(t))}))();export{s as formatter};
