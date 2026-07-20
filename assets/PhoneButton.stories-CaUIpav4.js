2e870793daa5e34d99c5572a0b5428c520c2d006
Amsterdam CI <app@amsterdam.nl>
2026-07-20 08:58:47 +0000

    upload 1.29.0.26072004

--- assets/PhoneButton.stories-CaUIpav4.js
+++
@@ -1,5 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./PhoneButton-DJ1LYZ65.js";var r,i,a;e((()=>{t(),r={component:n,argTypes:{onPress:{action:`onPress`}}},i={args:{phoneNumber:`0610000000`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    phoneNumber: '0610000000'
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
