66e94ca1acb596df396584442455f20c05034cbc
Amsterdam CI <app@amsterdam.nl>
2026-07-21 12:21:42 +0000

    upload 1.29.0.26072104

--- assets/CharactersLeftDisplay.stories-BGWQ7EEC.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./CharactersLeftDisplay-BssBNAMV.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{maxCharacters:50,numOfCharacters:0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    maxCharacters: 50,
-    numOfCharacters: 0
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
---
+++ assets/Label.stories-VOalOoFN.js
@@ -0,0 +1,5 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./Label-BS-PlizZ.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{text:`Wat is de titel van je bericht?`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    text: 'Wat is de titel van je bericht?'
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
