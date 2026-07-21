cdbb68bdf798611572f558c43e68896a13af72f3
Amsterdam CI <app@amsterdam.nl>
2026-07-21 12:17:12 +0000

    upload 1.29.0.26072102

--- assets/ErrorMessage.stories-Dtp8bPIc.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./ErrorMessage-CsKjekHE.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{testID:`testIDMessage`,text:`Vul een waarde in`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    testID: 'testIDMessage',
-    text: 'Vul een waarde in'
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
