60cbfb88faf26a86f1aa75386a3cc6931a62a9bc
Amsterdam CI <app@amsterdam.nl>
2026-07-22 11:11:43 +0000

    upload 1.29.0.26072206

--- assets/isThisYear-DtpKIPxg.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{Lt as t,zt as n}from"./iframe-DEkTnTfb.js";var r,i=e((()=>{r=({amount:e,position:t=`start`,text:n})=>t===`start`?n.slice(0,e):t===`end`?n.slice(0,-e):n})),a,o=e((()=>{n(),a=e=>t(e).isSame(t(),`year`)}));export{i,a as n,r,o as t};
