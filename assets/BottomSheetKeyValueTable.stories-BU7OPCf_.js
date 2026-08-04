eecbf882eb952bafa60b8e4712d56376b2bcfe43
Amsterdam CI <app@amsterdam.nl>
2026-08-04 18:29:53 +0000

    upload 1.29.0.26080404

---
+++ assets/BottomSheetKeyValueTable.stories-BU7OPCf_.js
@@ -0,0 +1,35 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{t}from"./jsx-runtime-BBQGix-2.js";import{G as n,K as r,ct as i,n as a,st as o}from"./dist-NCr0OYBw.js";import{n as s,t as c}from"./useThemable-D4FT0DWq.js";import{n as l,t as u}from"./Title-D8x5oarG.js";import{n as d,t as f}from"./Column-CnRFf-ky.js";import{n as p,t as m}from"./BottomSheetLabelValueRow-BOh8WOJm.js";var h,g,_,v=e((()=>{a(),c(),h=t(),g=({color:e=`default`,height:t=`sm`})=>(0,h.jsx)(n,{style:s(_(e,t)).divider}),_=(e,t)=>({color:n,border:r})=>i.create({divider:{height:r.width[t],backgroundColor:n.box.border[e]}});try{g.displayName=`Divider`,g.__docgenInfo={description:``,displayName:`Divider`,filePath:`/Users/runner/work/1/s/src/components/ui/Divider.tsx`,methods:[],props:{color:{defaultValue:{value:`default`},declarations:[{fileName:`s/src/components/ui/Divider.tsx`,name:`TypeLiteral`}],description:``,name:`color`,required:!1,tags:{},type:{name:`enum`,raw:`"default" | "cityPass" | "emphasis" | "distinct" | "onGrey"`,value:[{value:`"default"`},{value:`"cityPass"`},{value:`"emphasis"`},{value:`"distinct"`},{value:`"onGrey"`}]}},height:{defaultValue:{value:`sm`},declarations:[{fileName:`s/src/components/ui/Divider.tsx`,name:`TypeLiteral`}],description:``,name:`height`,required:!1,tags:{},type:{name:`enum`,raw:`"sm" | "md" | "lg" | "xl"`,value:[{value:`"sm"`},{value:`"md"`},{value:`"lg"`},{value:`"xl"`}]}}},tags:{}}}catch{}})),y,b,x=e((()=>{y=(e,t)=>b(e)&&b(t)&&Object.entries(t).every(([t,n])=>Object.hasOwn(e,t)&&Array.isArray(n)?n.includes(typeof e[t]):typeof e[t]===n),b=e=>typeof e==`object`&&!!e})),S,C,w=e((()=>{p(),v(),d(),l(),x(),S=t(),C=({title:e,showDividers:t=!1,rows:n})=>(0,S.jsxs)(f,{gutter:`sm`,children:[!!e&&(0,S.jsx)(u,{level:`h5`,text:e}),n.map((e,n)=>y(e,{key:[`string`,`number`],value:[`string`,`number`]})?(0,S.jsxs)(S.Fragment,{children:[!!t&&n>0&&(0,S.jsx)(g,{color:`emphasis`,height:`md`},`BottomSheetKeyValueTable-${n}-divider`),(0,S.jsx)(m,{label:e.key,value:e.value},`BottomSheetKeyValueTable-${n}`)]}):null)]});try{C.displayName=`BottomSheetKeyValueTable`,C.__docgenInfo={description:``,displayName:`BottomSheetKeyValueTable`,filePath:`/Users/runner/work/1/s/src/components/features/bottom-sheet/BottomSheetKeyValueTable.tsx`,methods:[],props:{rows:{defaultValue:null,declarations:[{fileName:`s/src/components/features/bottom-sheet/BottomSheetKeyValueTable.tsx`,name:`TypeLiteral`}],description:``,name:`rows`,required:!0,tags:{},type:{name:`{ key: string | number; value: string | number; }[]`}},showDividers:{defaultValue:{value:`false`},declarations:[{fileName:`s/src/components/features/bottom-sheet/BottomSheetKeyValueTable.tsx`,name:`TypeLiteral`}],description:``,name:`showDividers`,required:!1,tags:{},type:{name:`boolean`}},title:{defaultValue:null,declarations:[{fileName:`s/src/components/features/bottom-sheet/BottomSheetKeyValueTable.tsx`,name:`TypeLiteral`}],description:``,name:`title`,required:!1,tags:{},type:{name:`string | null`}}},tags:{}}}catch{}})),T,E,D,O,k;e((()=>{w(),T={component:C},E={args:{rows:[{key:`Key 1`,value:`Value 1`},{key:`Key 2`,value:`Value 2`}],title:`Key Value Table`}},D={args:{rows:[{key:`Key 1`,value:`Value 1`},{key:`Key 2`,value:`Value 2`}],title:void 0}},O={args:{rows:[{key:`Key 1`,value:`Value 1`},{key:`Key 2`,value:`Value 2`}],title:`Key Value Table`,showDividers:!0}},E.parameters={...E.parameters,docs:{...E.parameters?.docs,source:{originalSource:`{
+  args: {
+    rows: [{
+      key: 'Key 1',
+      value: 'Value 1'
+    }, {
+      key: 'Key 2',
+      value: 'Value 2'
+    }],
+    title: 'Key Value Table'
+  }
+}`,...E.parameters?.docs?.source}}},D.parameters={...D.parameters,docs:{...D.parameters?.docs,source:{originalSource:`{
+  args: {
+    rows: [{
+      key: 'Key 1',
+      value: 'Value 1'
+    }, {
+      key: 'Key 2',
+      value: 'Value 2'
+    }],
+    title: undefined
+  }
+}`,...D.parameters?.docs?.source}}},O.parameters={...O.parameters,docs:{...O.parameters?.docs,source:{originalSource:`{
+  args: {
+    rows: [{
+      key: 'Key 1',
+      value: 'Value 1'
+    }, {
+      key: 'Key 2',
+      value: 'Value 2'
+    }],
+    title: 'Key Value Table',
+    showDividers: true
+  }
+}`,...O.parameters?.docs?.source}}},k=[`Default`,`WithoutTitle`,`WithDividers`]}))();export{E as Default,O as WithDividers,D as WithoutTitle,k as __namedExportsOrder,T as default};
---
+++ assets/react-CSpbIcRZ.js
@@ -0,0 +1,1 @@
+import{i as e,n as t,r as n,t as r}from"./DocsRenderer-JROSPFPF-DV0W1bf-.js";r();export{t as MDXProvider};
