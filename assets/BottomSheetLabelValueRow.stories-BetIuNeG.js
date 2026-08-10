8d47ecbfadad6d09acc50f398a932d4c941d29fc
Amsterdam CI <app@amsterdam.nl>
2026-08-10 09:38:40 +0000

    upload 1.29.0.26081003

---
+++ assets/BottomSheetLabelValueRow.stories-BetIuNeG.js
@@ -0,0 +1,6 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./BottomSheetLabelValueRow-CgjxmCyZ.js";var r,i,a;e((()=>{t(),r={component:n},i={args:{label:`Prijs`,value:`€ 7,67`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Prijs',
+    value: '€ 7,67'
+  }
+}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
