120a48d4843772809944fcc622bbdb3a6d807ff8
Amsterdam CI <app@amsterdam.nl>
2026-09-02 12:50:27 +0000

    upload 1.30.0.26090204

--- assets/CustomMarkerIcon.stories-Cxgt-oru.js
+++
@@ -1,9 +0,0 @@
-import{c as e,i as t}from"./preload-helper-B45gAKPr.js";import{t as n}from"./react-D0Y92gk6.js";import{t as r}from"./jsx-runtime-BBQGix-2.js";import{n as i,t as a}from"./themes-C7yDjynq.js";import{n as o,t as s}from"./Column-DwlMsa4v.js";import{n as c,t as l}from"./Row-DrJ4o8pY.js";import{n as u,t as d}from"./CustomMarkerIcon-D9OHSP7M.js";import{r as f,t as p}from"./types-CpSvn75W.js";import{n as m,t as h}from"./boatChargingPointStateMap-CAlx6ZAl.js";var g,_,v,y,b;t((()=>{u(),o(),c(),m(),f(),a(),g=e(n()),_=r(),v={component:d,parameters:{backgrounds:{default:`custom-grey0`}}},y={render:e=>(0,_.jsx)(s,{gutter:`md`,children:[h[p.free],h[p.occupied],h[p.malfunction]].map(({icon:t})=>(0,_.jsx)(l,{gutter:`md`,children:Object.values(i.light.size.spacing).map(n=>(0,g.createElement)(d,{...e,icon:t,key:`${t.path}-${n}`,size:n}))},t.path))})},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
-  render: args => <Column gutter="md">
-      {[boatChargingPointStateMap[BoatChargingPointState.free], boatChargingPointStateMap[BoatChargingPointState.occupied], boatChargingPointStateMap[BoatChargingPointState.malfunction]].map(({
-      icon
-    }) => <Row gutter="md" key={icon.path}>
-          {Object.values(themes.light.size.spacing).map(size => <CustomMarkerIcon {...args} icon={icon} key={\`\${icon.path}-\${size}\`} size={size} />)}
-        </Row>)}
-    </Column>
-}`,...y.parameters?.docs?.source}}},b=[`Default`]}))();export{y as Default,b as __namedExportsOrder,v as default};
---
+++ assets/EmptyList.stories-BXMtv9pj.js
@@ -0,0 +1,9 @@
+import{i as e}from"./preload-helper-B45gAKPr.js";import{t}from"./jsx-runtime-BBQGix-2.js";import{n,t as r}from"./Size-DH-J11fw.js";import{n as i,t as a}from"./EmptyList-VOeTZdDF.js";var o,s,c,l;e((()=>{n(),i(),o=t(),s={component:a},c={args:{testID:`StorybookEmptyList`,text:`Geen resultaten`},render:e=>(0,o.jsx)(r,{height:500,children:(0,o.jsx)(a,{...e})})},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    testID: 'StorybookEmptyList',
+    text: 'Geen resultaten'
+  },
+  render: props => <Size height={500}>
+      <EmptyList {...props} />
+    </Size>
+}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
