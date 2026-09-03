2acbfc0132378e83d1e1829269ed93ccd548044f
Amsterdam CI <app@amsterdam.nl>
2026-09-03 11:32:41 +0000

    upload 1.30.0.26090301

--- assets/Checkbox.stories-Ba0KElxb.js
+++
@@ -1,7 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{n as t,t as n}from"./Checkbox-C8xgjdqW.js";var r,i,a;e((()=>{t(),r={component:n,argTypes:{onPress:{action:`onPress`}}},i={args:{label:`Ik ga akkoord met de voorwaarden`,labelPosition:`end`,isSelected:!1}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    label: 'Ik ga akkoord met de voorwaarden',
-    labelPosition: 'end',
-    isSelected: false
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Default`]}))();export{i as Default,a as __namedExportsOrder,r as default};
---
+++ assets/useDeviceContext-B-I2h-0k.js
@@ -0,0 +1,1 @@
+import{c as e,i as t}from"./preload-helper-B45gAKPr.js";import{t as n}from"./react-D0Y92gk6.js";import{in as r,rn as i}from"./iframe-BHfxSmuf.js";var a,o,s=t((()=>{a=e(n()),r(),o=()=>(0,a.use)(i)}));export{o as n,s as t};
