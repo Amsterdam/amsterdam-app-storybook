a02172b2d345875e5e0ed6bd8e20ba844addcfc5
Amsterdam CI <app@amsterdam.nl>
2026-09-11 15:22:17 +0000

    upload 1.30.0.26091104

--- assets/Tabs.stories-BzN9RxqV.js
+++
@@ -1,6 +0,0 @@
-import{i as e}from"./preload-helper-B45gAKPr.js";import{t}from"./jsx-runtime-BBQGix-2.js";import{n,t as r}from"./Phrase-BpyuxxTw.js";import{n as i,t as a}from"./Tabs-TmEK0Rkn.js";var o,s,c,l;e((()=>{i(),n(),o=t(),s={component:a,render:e=>(0,o.jsxs)(a,{...e,children:[(0,o.jsx)(a.Tab,{label:`Tab 1`,children:(0,o.jsx)(r,{children:`Content 1`})}),(0,o.jsx)(a.Tab,{label:`Tab 2`,children:(0,o.jsx)(r,{children:`Content 2`})})]})},c={args:{testID:`Tabs`,children:void 0}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
-  args: {
-    testID: 'Tabs',
-    children: undefined
-  }
-}`,...c.parameters?.docs?.source}}},l=[`Default`]}))();export{c as Default,l as __namedExportsOrder,s as default};
